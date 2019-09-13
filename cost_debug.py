e2eef3429e6a3237bb96318a57ad3f98b07cab35
John Bannister <jbannister@airsci.com>
2019-09-13 12:06:29 -0700

    contniuing to build

--- cost_debug.py
+++ cost_debug.py
@@ -1,6 +1,10 @@
 #%load_ext autoreload
 #%autoreload 2
 
-from pricer_import import Budgetize
-budget = Budgetize(budget_file="pricer_upload.xlsm")
+from pricer_import import Budget, Rates
+
+budget = Budget(budget_file="pricer_upload.xlsm")
 budget.pull_pricer()
+
+rates = Rates(rate_file="rate_sheet.xlsx")
+rates.pull_rates()
--- pricer_import.py
+++ pricer_import.py
@@ -1,39 +1,45 @@
 #! /usr/bin/env python
-#import time
 import pandas as pd
 import numpy as np
 import datetime as dt
 import os
 import re
+import pdb
+import psycopg2
 #from openpyxl import worksheet
 #from openpyxl import load_workbook
 #from openpyxl.utils.dataframe import dataframe_to_rows
-import pdb
 
-class Budgetize:
+class Budget:
     def __init__(self, budget_file):
         self.budget_file=budget_file
+        self.pricer=None
+        self.task_info=None
+        self.sub_info=None
+        self.labor_info=None
+        self.travel_info=None
+        self.expenses_info=None
+
 
     def parse_task_info(self):
         task_text = self.pricer.parse(sheet_name="Task Order Setup", header=None, usecols="C",
                 nrows=3)
-        task_info = pd.DataFrame({
-            'agree_no':re.findall('\d{5}[A-Z]', txt.iloc[1, 0]),
-            'proj_no':re.findall('\d{3}$', txt.iloc[1, 0]),
-            'task_no':re.findall('\d+', txt.iloc[0, 0])[0],
-            'task_name':txt.iloc[0, 0].split(":")[1].strip(),
-            'mod_no':re.findall('\d+', txt.iloc[0, 0])[1],
-            'start_date':dt.datetime.strptime(re.findall('\d{2}/\d{2}/\d{2}', txt.iloc[2, 0])[0],
+        task_info = {'agree_no':re.findall('\d{5}[A-Z]', task_text.iloc[1, 0]),
+            'proj_no':re.findall('\d{3}$', task_text.iloc[1, 0]),
+            'task_no':re.findall('\d+', task_text.iloc[0, 0])[0],
+            'task_name':task_text.iloc[0, 0].split(":")[1].strip(),
+            'mod_no':re.findall('\d+', task_text.iloc[0, 0])[1],
+            'start_date':dt.datetime.strptime(re.findall('\d{2}/\d{2}/\d{2}', task_text.iloc[2, 0])[0],
                 '%m/%d/%y'),
-            'end_date':dt.datetime.strptime(re.findall('\d{2}/\d{2}/\d{2}', txt.iloc[2, 0])[1],
-                '%m/%d/%y')})
+            'end_date':dt.datetime.strptime(re.findall('\d{2}/\d{2}/\d{2}', task_text.iloc[2, 0])[1],
+                '%m/%d/%y')}
         return task_info
 
     def parse_sub_info(self):
         sub_info = self.pricer.parse(sheet_name="Task Order Setup", header=None, skiprows=6,
                 usecols="B,C", names=["sub_no", "sub_name"])
         sub_info.dropna(how='any', inplace=True)
-        sub_info = self.sub_info.loc[[not x=='.' for x in self.sub_info['sub_name']], :]
+        sub_info = sub_info.loc[[not x=='.' for x in sub_info['sub_name']], :]
         return(sub_info)
 
     def parse_labor(self):
@@ -46,7 +52,7 @@ class Budgetize:
         labor = labor.iloc[0:labor.iloc[:, 2].tolist().index('.')-3, 0:personnel_end_index]
         labor = labor.loc[[not x==0 for x in labor.loc[:, 4]], :]
         labor = labor.loc[[not np.isnan(x) for x in labor.iloc[:, 1]]]
-        labor.iloc[:, 0] = [int(x) for x in labor.iloc[:, 1]]
+        labor.iloc[:, 0] = [int(float(x)) for x in labor.iloc[:, 1]]
 
         labor_info = pd.DataFrame({'sub_no':labor.iloc[:, 0], 'work_no':labor.iloc[:, 1],
             'work_name':labor.iloc[:, 2], 'deliv_name':labor.iloc[:, 3],
@@ -62,13 +68,13 @@ class Budgetize:
         travel = self.pricer.parse(sheet_name="Travel", header=None, skiprows=22, usecols="A,B,D,U")
         travel.dropna(how='any', inplace=True)
         travel.columns = ['travel_no', 'company', 'desc', 'cost']
-        travel['sub_no'] = [int(x) for x in travel['travel_no']]
+        travel['sub_no'] = [int(float(x)) for x in travel['travel_no']]
 
     def parse_expenses(self):
         expenses = self.pricer.parse(sheet_name="Expenses", header=None, skiprows=18, usecols="A,B,D,G")
         expenses.dropna(how='any', inplace=True)
         expenses.columns = ['expense_no', 'company', 'desc', 'cost']
-        expenses['sub_no'] = [int(x) for x in expenses['expense_no']]
+        expenses['sub_no'] = [int(float(x)) for x in expenses['expense_no']]
 
     def pull_pricer(self):
 
@@ -83,3 +89,62 @@ class Budgetize:
         self.travel_info = self.parse_travel()
 
         self.expenses_info = self.parse_expenses()
+
+class Rates:
+    def __init__(self, rate_file):
+        self.rate_file=rate_file
+        self.rates=None
+        self.rate_info=None
+        self.staff_levels=None
+        self.level_rates=None
+
+        self.conn = psycopg2.connect(user='airsci', host='localhost', port='5432', database='cost_control')
+        self.cursor = self.conn.cursor()
+
+    def parse_rate_info(self):
+        rate_text = self.rates.parse(sheet_name="Rates", header=None, skiprows=2, usecols="B", nrows=2)
+        rate_info = {'company':rate_text.iloc[0, 0].split(":")[1].strip(),
+            'effect_date':dt.datetime.strptime(re.findall('\d{2}/\d{2}/\d{2}', rate_text.iloc[1, 0])[0], '%m/%d/%y')}
+        comp_abrv = "".join([word[0] for word in rate_info['company'].split()])
+        rate_info['sheet_code'] = comp_abrv + "-" + rate_info['effect_date'].strftime("%y%m%d")
+        return rate_info
+
+    def parse_staff_levels(self):
+        staff_levels = self.rates.parse(sheet_name="Rates", header=None, skiprows=7, usecols="A,B")
+        staff_levels.dropna(how='any', inplace=True)
+        staff_levels.columns = ['name', 'level']
+        return(staff_levels)
+
+    def parse_level_rates(self):
+        level_rates = self.rates.parse(sheet_name="Rates", header=None, skiprows=7, usecols="D,E")
+        level_rates.dropna(how='any', inplace=True)
+        level_rates.columns = ['level', 'rate']
+        return(level_rates)
+
+    def pull_rates(self):
+
+        self.rates = pd.ExcelFile(os.getcwd() + "/" + self.rate_file)
+
+        self.rate_info = self.parse_rate_info()
+        self.staff_levels = self.parse_staff_levels()
+        self.level_rates = self.parse_level_rates()
+
+
+df1 = pd.read_csv("~/Desktop/dump/comps.csv", header=None)
+
+def insert_row(table, values, columns=None):
+    if columns==None:
+        sql = "INSERT INTO %s VALUES(%s);" % (table, str(values.tolist())[1:-1])
+    else:
+        sql = "INSERT INTO %s(%s) VALUES(%s);" % (table, str(columns)[1:-1].replace("\'", ""), str(values)[1:-1])
+
+    try:
+        self.cursor.execute(sql)
+    except IntegrityError as error:
+        print(error)
+    
+    self.conn.commit()
+
+
+
+
